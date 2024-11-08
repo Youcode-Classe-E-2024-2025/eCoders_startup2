646194ad551f1a3153e8331d2e56b3d71bbc74a2
yassine snaiki <yassnaiki@gmail.com>
2024-11-08 12:51:16 +0100

    navigation bar and footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
     content: ["./index.html", "./assets/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            backgroundImage: {
+                'split-70-30': 'linear-gradient(to right, #F9F1E7 70%, #FCF8F3 70%)',
+              },
+        },
     },
     plugins: [],
 };
