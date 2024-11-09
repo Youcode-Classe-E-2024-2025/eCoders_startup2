bd252076b31aa43275b7859fdbe3d5cd742ed11a
medlaq777 <m.laqsoumi@gmail.com>
2024-11-09 16:35:52 +0100

    Add initial CSS styling for login page layout; positioned two input fields within the background image

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./index.html", "./assets/**/*.{html,js}"],
-    theme: {
-        extend: {
-            backgroundImage: {
-                'split-70-30': 'linear-gradient(to right, #F9F1E7 70%, #FCF8F3 70%)',
-              },
-        },
+  content: ["./index.html", "./assets/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "split-70-30": "linear-gradient(to right, #F9F1E7 70%, #FCF8F3 70%)",
+      },
     },
-    plugins: [],
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+    },
+  },
+  plugins: [],
 };
