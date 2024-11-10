a2375738fa5d1438a94e2d200750db6e67ad99ed
Khawla Boukniter <boukniter.khawla@gmail.com>
2024-11-10 21:31:16 +0100

    Ajouter la fonction updatePrice pour mettre à jour les infos de prix en fonction de nombre des produits et quantité

--- assets/pages/cart/cart.js
+++ assets/pages/cart/cart.js
@@ -10,8 +10,33 @@ document.addEventListener("DOMContentLoaded", () => {
         count.textContent = `You have ${countItems} items in your cart`;
     }
 
+    function updatePrice() {
+        let subT = 0;
+        document.querySelectorAll(".cart-item").forEach(el => {
+            const quantity = parseInt(el.querySelector(".quantity").value) || 0;
+            const itemPrice = parseFloat(el.dataset.price) || 0;
+            const totalPrice = quantity * itemPrice;
+            
+
+            console.log(`Quantity: ${quantity}, Item Price: ${itemPrice}, Total Price: ${totalPrice}`);
+
+            el.querySelector(".item-price").textContent = `$${totalPrice.toFixed(2)}`;
+
+            subT += totalPrice;
+        });
+
+        console.log(`Subtotal: ${subT}, Total (with shipping): ${subT + shipping}`);
+        
+        subtotalEl.textContent = `$${subT.toFixed(2)}`;
+        // const shipping = 42;
+        totalEl.textContent = `$${subT + shipping}`;
+
+        checkoutPrice.textContent = `$${(subT + shipping)}`
+    }
+
     
 
     updateCount();
+    updatePrice();
 
 });
