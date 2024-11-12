f018d5b8ad7d034c7c24f387507a94faeff0c416
medlaq777 <m.laqsoumi@gmail.com>
2024-11-12 16:45:03 +0100

    update config tailwind and style.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
       backgroundImage: {
         "split-70-30": "linear-gradient(to right, #F9F1E7 70%, #FCF8F3 70%)",
       },
+      colors: {
+        itemGreen: "#B7D0C1",
+        itemGris: "#CCC8C9",
+      },
     },
   },
   plugins: [],
