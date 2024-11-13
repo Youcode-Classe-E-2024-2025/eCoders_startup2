3739392726040321709b59ed778fe1a1a253e6a8
JIHANE ELMAJDI <emleyamanda@gmail.com>
2024-11-13 17:01:22 +0100

    Ajout de la section 'FAQ'

---
+++ assets/pages/about/about.js
@@ -0,0 +1,27 @@
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+  faq.addEventListener("click", () => {
+    faqs.forEach((item) => {
+      const reponse = item.querySelector(".reponse");
+      const icone = item.querySelector("i");
+
+      if (item !== faq) {
+        reponse.style.display = "hidden";
+        icone.classList.add("fa-circle-up");
+        icone.classList.remove("fa-circle-down");
+      }
+    });
+
+    const reponse = faq.querySelector(".reponse");
+    if (reponse.classList.contains("hidden")) {
+      reponse.classList.remove("hidden");
+    } else {
+      reponse.classList.add("hidden");
+    }
+
+    const icone = faq.querySelector("i");
+    icone.classList.toggle("fa-circle-up");
+    icone.classList.toggle("fa-circle-down");
+  });
+});
