56e4d590540b5e0d73cb3894b6463e792d5b4097
Khawla Boukniter <boukniter.khawla@gmail.com>
2024-11-13 10:16:56 +0100

    Adding new folder to arrange data

---
+++ assets/Data/data.js
@@ -0,0 +1,177 @@
+const espadrilles = {
+    nike: [
+      { 
+        name: "Air Max", 
+        price: 130.99, 
+        description: "Confort et style emblématique.", 
+        quantity: 10, 
+        color: "#FFFFFF" 
+      },
+      { 
+        name: "Classic", 
+        price: 120.50, 
+        description: "Look intemporel pour toutes occasions.", 
+        quantity: 15, 
+        color: "#000000" 
+      },
+      { 
+        name: "Court Legacy", 
+        price: 110, 
+        description: "Légère et décontractée.", 
+        quantity: 8, 
+        color: "#00FF00" 
+      },
+      { 
+        name: "SB Flex", 
+        price: 100.75, 
+        description: "Performance et confort en un.", 
+        quantity: 12, 
+        color: "#808080" 
+      },
+      { 
+        name: "Zoom Air", 
+        price: 140, 
+        description: "Amorti supérieur et design moderne.", 
+        quantity: 9, 
+        color: "#D3D3D3" 
+      },
+      { 
+        name: "Revolution", 
+        price: 95.25, 
+        description: "Respirante et polyvalente.", 
+        quantity: 11, 
+        color: "#0000FF" 
+      },
+      { 
+        name: "Flex Runner", 
+        price: 85, 
+        description: "Légère pour une course optimale.", 
+        quantity: 14, 
+        color: "#000000" 
+      },
+      { 
+        name: "Quest", 
+        price: 125.49, 
+        description: "Excellent maintien et design moderne.", 
+        quantity: 10, 
+        color: "#FF0000" 
+      }
+    ],
+    adidas: [
+      { 
+        name: "Gazelle", 
+        price: 100.00, 
+        description: "Style rétro et confortable.", 
+        quantity: 10, 
+        color: "#FFFFFF" 
+      },
+      { 
+        name: "Stan Smith", 
+        price: 130, 
+        description: "Élégante pour tous les styles.", 
+        quantity: 12, 
+        color: "#00FF00" 
+      },
+      { 
+        name: "NMD", 
+        price: 150.99, 
+        description: "Touche urbaine et performance.", 
+        quantity: 9, 
+        color: "#808080" 
+      },
+      { 
+        name: "Superstar", 
+        price: 90.30, 
+        description: "Classique et intemporelle.", 
+        quantity: 8, 
+        color: "#FFFFFF" 
+      },
+      { 
+        name: "Ultraboost", 
+        price: 180.49, 
+        description: "Parfaite pour de longues marches.", 
+        quantity: 7, 
+        color: "#0000FF" 
+      },
+      { 
+        name: "Lite Racer", 
+        price: 85.00, 
+        description: "Légère et stylée.", 
+        quantity: 13, 
+        color: "#FF0000" 
+      },
+      { 
+        name: "Duramo", 
+        price: 90, 
+        description: "Polyvalente et confortable.", 
+        quantity: 11, 
+        color: "#000080" 
+      },
+      { 
+        name: "Samba", 
+        price: 95.75, 
+        description: "Style décontracté et classique.", 
+        quantity: 12, 
+        color: "#A52A2A" 
+      }
+    ],
+    newBalance: [
+      { 
+        name: "574", 
+        price: 110.25, 
+        description: "Confort et design rétro.", 
+        quantity: 10, 
+        color: "#808080" 
+      },
+      { 
+        name: "997", 
+        price: 120, 
+        description: "Confort pour une utilisation quotidienne.", 
+        quantity: 9, 
+        color: "#000080" 
+      },
+      { 
+        name: "990", 
+        price: 160.49, 
+        description: "Haute qualité et amorti supérieur.", 
+        quantity: 8, 
+        color: "#000000" 
+      },
+      { 
+        name: "550", 
+        price: 110, 
+        description: "Style moderne et polyvalent.", 
+        quantity: 11, 
+        color: "#D3D3D3" 
+      },
+      { 
+        name: "Fresh Foam", 
+        price: 130.75, 
+        description: "Légèreté et confort optimal.", 
+        quantity: 10, 
+        color: "#FFFFFF" 
+      },
+      { 
+        name: "1080", 
+        price: 140.99, 
+        description: "Pour des performances sportives.", 
+        quantity: 8, 
+        color: "#FFFF00" 
+      },
+      { 
+        name: "237", 
+        price: 105.49, 
+        description: "Élégante pour un usage quotidien.", 
+        quantity: 12, 
+        color: "#FF0000" 
+      },
+      { 
+        name: "373", 
+        price: 95.99, 
+        description: "Confort et style classique.", 
+        quantity: 14, 
+        color: "#FFFFFF" 
+      }
+    ]
+  };
+  
