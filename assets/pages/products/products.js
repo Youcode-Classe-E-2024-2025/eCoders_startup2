81c7801d4d114f5bafcbae47d7a4c4864a0b6932
echchabli hamza <echchablihamza1@gmail.com>
2024-11-13 11:43:33 +0100

    add photos and fixing there description

--- assets/pages/products/products.js
+++ assets/pages/products/products.js
@@ -24,16 +24,22 @@ function cardDisplay(product) {
                     <h2 class="productPrice font-semibold text-3xl">$${product.price}</h2>
                 </div>
                 <div class="absolute inset-0 bg-[#000000] opacity-0 group-hover:opacity-50 transition-opacity duration-300"></div>
-               <div class="cardButtons absolute inset-0 flex pt-7 flex-col items-center justify-center opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+               <div class="cardButtons absolute inset-0 flex pt-7 flex-col  justify-center gap-3 opacity-0 transition-opacity duration-300 group-hover:opacity-100">
                      <!-- Button with a link to the cart -->
-                     <a href="cart.html" class="w-full">
-                         <button class="cardButtons bg-white text-[#E89F71] w-[70%] py-2 m-2 border font-medium" aria-label="Add to Cart">Add to Cart</button>
-                     </a>
+                     <a href="../cart/cart.html?ID=${product.id}" class=" w-full flex justify-center mb-3">
+    <button class="cardButtons bg-white text-[#E89F71] w-[70%] py-2 m-2 border font-medium" aria-label="Add to Cart">
+        Add to Cart
+    </button>
+</a>
+
 
                      <!-- Button with a link to the product details page -->
-                     <a href="product-details.html?productId=123" class="w-full">
-                         <button class="cardButtons bg-white text-[#E89F71] w-[70%] py-2 m-2 border font-medium" aria-label="See Details">See Details</button>
-                     </a>
+                     <a href="../details/details.html?Id=33" class="w-full flex justify-center">
+    <button class="cardButtons bg-white text-[#E89F71] w-[70%] py-2 border font-medium" aria-label="See Details">
+        See Details
+    </button>
+</a>
+
                </div>
 
             </div>
