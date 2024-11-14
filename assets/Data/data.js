32d3dd5ab235e8d589eac155844362eb44a11efc
Khawla Boukniter <boukniter.khawla@gmail.com>
2024-11-14 11:24:57 +0100

    Updating paths in data.js

--- assets/Data/data.js
+++ assets/Data/data.js
@@ -1,5 +1,3 @@
-
-
 export const products = [
     {id: 1, name: "990v5", price: 166.43, img: "NB1.webp", gallerie: ["NB11.webp", "NB12.webp", "NB13.webp"], description: "sport shoe", category: "New Balance", quantity: 10, colors: ["#00443F", "#B9975E", "#3C6E72", "#E5E4DB"]},
     {id: 2, name: "Foam 1080", price: 189.99, img: "NB2.webp", gallerie: ["NB21.webp", "NB22.webp", "NB23.webp"], description: "running shoe", category: "New Balance", quantity: 5, colors: ["#111315"]},
@@ -15,33 +13,32 @@ export const products = [
     {id: 12, name: "574 Core", price: 600.00, img: "NB120.webp", gallerie: ["NB121.webp", "NB122.webp", "NB123.webp"], description: " everyday shoe", category: "New Balance", quantity: 4, colors: ["#101014"]},
     
     //nike
-    
-    {id: 13, name: "Air Max", price: 130.99, img: "Nike-AIRMAX.png", gallerie: ["nike/Nike-AIRMAX-1.jpg", "nike/Nike-AIRMAX-2.jpg", "nike/Nike-AIRMAX-3.png"], description: "Confort et style", category: "nike", quantity: 10, colors: ["#FFFFFF"]},
-    {id: 14, name: "Classic", price: 120.50, img: "CLASSIC.png", gallerie: ["nike/CLASSIC-1.jpg", "nike/CLASSIC-2.jpg", "nike/CLASSIC-3.jpg"], description: "Look intemporel", category: "nike", quantity: 15, colors: ["#000000"]},
-    {id: 15, name: "Court L", price: 110.00, img: "COURT-LEGACY.png", gallerie: ["nike/COURT-LEGACY-1.png", "nike/COURT-LEGACY-2.png", "nike/COURT-LEGACY-3.png"], description: "Légère et décontractée.", category: "nike", quantity: 8, colors: ["#00FF00"]},
-    {id: 16, name: "SB Flex", price: 100.75, img: "SB-FORCE.png", gallerie: ["nike/SB-FORCE-1.jpg", "nike/SB-FORCE-2.jpg", "nike/SB-FORCE-3.png"], description: "Confort pour skateurs.", category: "nike", quantity: 10, colors: ["#FF0000"]},
-    {id: 17, name: "Zoom Air", price: 140, img:"ZOOM.png", gallerie: ["nike/ZOOM-1.png", "nike/ZOOM-2.png", "nike/ZOOM-3.png"], description: "design moderne.", category: "nike", quantity: 9, colors: ["#D3D3D3"]},
+    {id: 13, name: "Air Max", price: 130.99, img: "Nike-AIRMAX.png", gallerie: ["Nike-AIRMAX-1.jpg", "Nike-AIRMAX-2.jpg", "Nike-AIRMAX-3.png"], description: "Confort et style", category: "nike", quantity: 10, colors: ["#FFFFFF"]},
+    {id: 14, name: "Classic", price: 120.50, img: "CLASSIC.png", gallerie: ["CLASSIC-1.jpg", "CLASSIC-2.jpg", "CLASSIC-3.jpg"], description: "Look intemporel", category: "nike", quantity: 15, colors: ["#000000"]},
+    {id: 15, name: "Court L", price: 110.00, img: "COURT-LEGACY.png", gallerie: ["COURT-LEGACY-1.png", "COURT-LEGACY-2.png", "COURT-LEGACY-3.png"], description: "Légère et décontractée.", category: "nike", quantity: 8, colors: ["#00FF00"]},
+    {id: 16, name: "SB Flex", price: 100.75, img: "SB-FORCE.png", gallerie: ["SB-FORCE-1.jpg", "SB-FORCE-2.jpg", "SB-FORCE-3.png"], description: "Confort pour skateurs.", category: "nike", quantity: 10, colors: ["#FF0000"]},
+    {id: 17, name: "Zoom Air", price: 140, img:"ZOOM.png", gallerie: ["ZOOM-1.png", "ZOOM-2.png", "ZOOM-3.png"], description: "design moderne.", category: "nike", quantity: 9, colors: ["#D3D3D3"]},
     {id: 18, name: "Revolution", price: 95.25, img: "REVOLUTION.jpg", gallerie: ["REVOLUTION-1.jpg", "REVOLUTION-2.jpg", "REVOLUTION-3.png"], description: "Respirante et Confort.", category: "nike", quantity: 11, colors: ["#0000FF"]},
-    {id: 19, name: "Flex Runner", price: 85, img: "FLEX-RUNNER.png", gallerie: ["nike/FLEX-RUNNER-1.png", "nike/FLEX-RUNNER-2.jpg", "nike/FLEX-RUNNER-3.png"], description: "Légère  optimale.", category: "nike", quantity: 14, colors: ["#000000"]},
-    {id: 20, name: "Quest", price: 125.49, img: "QUEST.png", gallerie: ["nike/QUEST-1.jpg", "nike/QUEST-2.jpg", "nike/QUEST-3.png"], description: "Confort et style", category: "nike", quantity: 10, colors: ["#FF0000"]},
-    {id:21, name: "Jordan", price: 90.99, img: "JORDAN-STADIUM.png", gallerie: ["nike/JORDAN-STADIUM-1.png", "nike/JORDAN-STADIUM-2.png", "nike/JORDAN-STADIUM-3.png"], description: "Chaussure pour homme.", category: "nike", quantity: 10, colors: ["#FF0000"]},
-    {id: 22, name: "Nike Force", price: 64.99, img: "FULLFORCE.png", gallerie: ["nike/FULLFORCE-1.png", "nike/FULLFORCE-2.png", "nike/FULLFORCE-3.png"], description: "Chaussure pour homme.", category: "nike", quantity: 10, colors: ["#FF0000"]}, 
-    {id: 23, name: "Attack", price: 97.99, img: "ATTACK.jpg", gallerie: ["nike/ATTACK-1.png", "nike/ATTACK-2.jpg", "nike/ATTACK-3.jpg"], description: "Chaussure pour homme.", category: "nike", quantity: 10, colors: ["#FF0000"]},
-    {id: 24, name: "Tech Hera", price: 125.49, img: "TECH-HERA.png", gallerie: ["nike/TECH-HERA-1.png", "nike/TECH-HERA-2.png", "nike/TECH-HERA-3.png"], description: "Chaussure pour homme.", category: "nike", quantity: 10, colors: ["#FF0000"]}, 
+    {id: 19, name: "Flex Runner", price: 85, img: "FLEX-RUNNER.png", gallerie: ["FLEX-RUNNER-1.png", "FLEX-RUNNER-2.jpg", "FLEX-RUNNER-3.png"], description: "Légère  optimale.", category: "nike", quantity: 14, colors: ["#000000"]},
+    {id: 20, name: "Quest", price: 125.49, img: "QUEST.png", gallerie: ["QUEST-1.jpg", "QUEST-2.jpg", "QUEST-3.png"], description: "Confort et style", category: "nike", quantity: 10, colors: ["#FF0000"]},
+    {id:21, name: "Jordan", price: 90.99, img: "JORDAN-STADIUM.png", gallerie: ["JORDAN-STADIUM-1.png", "JORDAN-STADIUM-2.png", "JORDAN-STADIUM-3.png"], description: "Chaussure pour homme.", category: "nike", quantity: 10, colors: ["#FF0000"]},
+    {id: 22, name: "Nike Force", price: 64.99, img: "FULLFORCE.png", gallerie: ["FULLFORCE-1.png", "FULLFORCE-2.png", "FULLFORCE-3.png"], description: "Chaussure pour homme.", category: "nike", quantity: 10, colors: ["#FF0000"]}, 
+    {id: 23, name: "Attack", price: 97.99, img: "ATTACK.jpg", gallerie: ["ATTACK-1.png", "ATTACK-2.jpg", "ATTACK-3.jpg"], description: "Chaussure pour homme.", category: "nike", quantity: 10, colors: ["#FF0000"]},
+    {id: 24, name: "Tech Hera", price: 125.49, img: "TECH-HERA.png", gallerie: ["TECH-HERA-1.png", "TECH-HERA-2.png", "TECH-HERA-3.png"], description: "Chaussure pour homme.", category: "nike", quantity: 10, colors: ["#FF0000"]}, 
 
     // Adidas
-    {id: 25, name: "Gazelle", price: 100.00, img: "gazelle.png", gallerie: ["adidas/gazelle-1.png", "adidas/gazelle-2.png", "adidas/gazelle-3.png"], description: " confortable.", category: "adidas", quantity: 10, colors: ["#FFFFFF"]},
-    {id: 26, name: "Stan Smith", price: 130, img: "Stan-Smith.png", gallerie: ["adidas/Stan-Smith-1.png", "adidas/Stan-Smith-2.png", "adidas/Stan-Smith-3.png"], description: "Élégante ", category: "adidas", quantity: 12, colors: ["#00FF00"]},   
-    {id: 27, name: "NMD", price: 150.99, img: "NMD.png", gallerie: ["adidas/NMD-1.png", "adidas/NMD-2.png", "adidas/NMD-3.png"], description: "urbaine et performance.", category: "adidas", quantity: 9, colors: ["#808080"]},
-    {id: 28, name: "Superstar", price: 90.30, img: "SuperStar.png", gallerie: ["adidas/SuperStar-1.png", "adidas/SuperStar-2.png", "adidas/SuperStar-3.png"], description: "Classique et confort.", category: "adidas", quantity: 8, colors: ["#FFFFFF"]},
-    {id: 29, name: "Ultraboost", price: 180.49, img: "Ultraboost.png", gallerie: ["adidas/Ultraboost-1.png", "adidas/Ultraboost-2.png", "adidas/Ultraboost-3.png"], description: "Parfaite pour marches.", category: "adidas", quantity: 7, color: "#0000FF" },
-    {id: 30, name: "Lite Racer", price: 85.00, img: "Lite-racer.png", gallerie: ["adidas/Lite-racer-1.png", "adidas/Lite-racer-2.png", "adidas/Lite-racer-3.png"], description: "Légère et stylée.", category: "adidas", quantity: 13, color: "#FF0000" }, 
-    {id: 31, name: "Duramo", price: 90, img: "Duramo.png", gallerie: ["adidas/Duramo-1.png", "adidas/Duramo-2.png", "adidas/Duramo-3.png"], description: "Classique et confort.", category: "adidas", quantity: 11, color: "#000080" },
-    {id: 32, name: "Samba", price: 95.75, img: "Samba.png", gallerie: ["adidas/Samba-1.png", "adidas/Samba-2.png", "adidas/Samba-3.png"], description: "Style classique.", category: "adidas", quantity: 12, color: "#A52A2A" }, 
-    {id: 33, name: "Racer TR23", price: 80, img: "Racer.png", gallerie: ["adidas/Racer-1.png", "adidas/Racer-2.png", "adidas/Racer-3.png"], description: "Style classique.", category: "adidas", quantity: 12, color: "#A52A2A" },
-    {id: 34, name: "Runfalcon", price: 65, img: "Runfalcon.png", gallerie: ["adidas/Runfalcon-1.png", "adidas/Runfalcon-2.png", "adidas/Runfalcon-3.png"], description: "Style classique.", category: "adidas", quantity: 12, color: "#A52A2A"},
-    {id: 35, name: "OZMILLEN", price: 110, img: "OZMILLEN.png", gallerie: ["adidas/OZMILLEN-1.png", "adidas/OZMILLEN-2.png", "adidas/OZMILLEN-3.png"], description: "Style classique.", category: "adidas", quantity: 12, color: "#A52A2A"},
-    {id: 36, name: "Kaptir", price: 90, img: "Kaptir.png", gallerie: ["adidas/Kaptir-1.png", "adidas/Kaptir-2.png", "adidas/Kaptir-3.png"], description: "Style classique.", category: "adidas", quantity: 12, color: "#A52A2A"}
+    {id: 25, name: "Gazelle", price: 100.00, img: "gazelle.png", gallerie: ["gazelle-1.png", "gazelle-2.png", "gazelle-3.png"], description: " confortable.", category: "adidas", quantity: 10, colors: ["#FFFFFF"]},
+    {id: 26, name: "Stan Smith", price: 130, img: "Stan-Smith.png", gallerie: ["Stan-Smith-1.png", "Stan-Smith-2.png", "Stan-Smith-3.png"], description: "Élégante ", category: "adidas", quantity: 12, colors: ["#00FF00"]},   
+    {id: 27, name: "NMD", price: 150.99, img: "NMD.png", gallerie: ["NMD-1.png", "NMD-2.png", "NMD-3.png"], description: "urbaine et performance.", category: "adidas", quantity: 9, colors: ["#808080"]},
+    {id: 28, name: "Superstar", price: 90.30, img: "SuperStar.png", gallerie: ["SuperStar-1.png", "SuperStar-2.png", "SuperStar-3.png"], description: "Classique et confort.", category: "adidas", quantity: 8, colors: ["#FFFFFF"]},
+    {id: 29, name: "Ultraboost", price: 180.49, img: "Ultraboost.png", gallerie: ["Ultraboost-1.png", "Ultraboost-2.png", "Ultraboost-3.png"], description: "Parfaite pour marches.", category: "adidas", quantity: 7, color: "#0000FF" },
+    {id: 30, name: "Lite Racer", price: 85.00, img: "Lite-racer.png", gallerie: ["Lite-racer-1.png", "Lite-racer-2.png", "Lite-racer-3.png"], description: "Légère et stylée.", category: "adidas", quantity: 13, color: "#FF0000" }, 
+    {id: 31, name: "Duramo", price: 90, img: "Duramo.png", gallerie: ["Duramo-1.png", "Duramo-2.png", "Duramo-3.png"], description: "Classique et confort.", category: "adidas", quantity: 11, color: "#000080" },
+    {id: 32, name: "Samba", price: 95.75, img: "Samba.png", gallerie: ["Samba-1.png", "Samba-2.png", "Samba-3.png"], description: "Style classique.", category: "adidas", quantity: 12, color: "#A52A2A" }, 
+    {id: 33, name: "Racer TR23", price: 80, img: "Racer.png", gallerie: ["Racer-1.png", "Racer-2.png", "Racer-3.png"], description: "Style classique.", category: "adidas", quantity: 12, color: "#A52A2A" },
+    {id: 34, name: "Runfalcon", price: 65, img: "Runfalcon.png", gallerie: ["Runfalcon-1.png", "Runfalcon-2.png", "Runfalcon-3.png"], description: "Style classique.", category: "adidas", quantity: 12, color: "#A52A2A"},
+    {id: 35, name: "OZMILLEN", price: 110, img: "OZMILLEN.png", gallerie: ["OZMILLEN-1.png", "OZMILLEN-2.png", "OZMILLEN-3.png"], description: "Style classique.", category: "adidas", quantity: 12, color: "#A52A2A"},
+    {id: 36, name: "Kaptir", price: 90, img: "Kaptir.png", gallerie: ["Kaptir-1.png", "Kaptir-2.png", "Kaptir-3.png"], description: "Style classique.", category: "adidas", quantity: 12, color: "#A52A2A"}
 
 ];
 
